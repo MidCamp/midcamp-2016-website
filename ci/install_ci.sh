570263ad21f5309df2f8b6af383cb99fbf1d26e2
Will Long <will@kerasai.com>
2015-08-23 14:45:07 -0500

    MIDCAMP-229: moved path adjustment

--- ci/install_ci.sh
+++ ci/install_ci.sh
@@ -4,6 +4,5 @@ cd $BASEDIR/ci
 mkdir bin
 composer install
 
-export PATH="$BASEDIR/ci/bin:$PATH"
 ln -s $BASEDIR/ci/vendor/drush/drush $BASEDIR/ci/bin/drush
 ln -s $BASEDIR/ci/vendor/phploc/phploc $BASEDIR/ci/bin/phploc
