39743b513fbff1d5aa19b3b164ea03ca49e33dc9
Will Long <will@kerasai.com>
2015-08-24 07:54:11 -0500

    MIDCAMP-229: added file analysis to config_vhost.sh

--- ci/config_vhost.sh
+++ ci/config_vhost.sh
@@ -7,5 +7,7 @@ DOCROOT=$2
 TEMPLATE=$3
 
 sed 's|{{DOCROOT}}|${DOCROOT}|' ${TEMPLATE} > /etc/apache2/sites-available/${CONFIG_NAME}.conf
+ls  /etc/apache2/sites-available
+cat /etc/apache2/sites-available/${CONFIG_NAME}.conf
 a2ensite $CONFIG_NAME
 service apache2 restart
