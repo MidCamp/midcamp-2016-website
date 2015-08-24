47ec6bf6119ac5661e0f516e4732d2a831432348
Will Long <will@kerasai.com>
2015-08-24 05:42:42 -0500

    MIDCAMP-229: adjusted apache install, added phpcs

--- ci/install_apache.sh
+++ ci/install_apache.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+apt-get update > /dev/null
+apt-get install -y --force-yes apache2 libapache2-mod-php5 php5-curl php5-mysql
+
+a2dissite 000-default
+a2enmod rewrite
+
+# Install site.
 sed 's|!DOCROOT!|$BASEDIR/docroot|' <$BASEDIR/ci/apache/vhost.conf >/etc/apache2/sites-available/midcamp.conf
 a2ensite midcamp
+
 service apache2 restart
