7c240f81b12ecccd165d7ae7163e1b7661e1e743
Will Long <will@kerasai.com>
2015-08-24 08:09:24 -0500

    MIDCAMP-229: adjusted vhost file name

--- ci/config_vhost.sh
+++ ci/config_vhost.sh
@@ -6,8 +6,8 @@ CONFIG_NAME=$1
 DOCROOT=$2
 TEMPLATE=$3
 
-sed "s|{{DOCROOT}}|${DOCROOT}|" ${TEMPLATE} > /etc/apache2/sites-available/${CONFIG_NAME}.conf
+sed "s|{{DOCROOT}}|${DOCROOT}|" ${TEMPLATE} > /etc/apache2/sites-available/${CONFIG_NAME}
 ls  /etc/apache2/sites-available
-cat /etc/apache2/sites-available/${CONFIG_NAME}.conf
+cat /etc/apache2/sites-available/${CONFIG_NAME}
 a2ensite $CONFIG_NAME
 service apache2 restart
