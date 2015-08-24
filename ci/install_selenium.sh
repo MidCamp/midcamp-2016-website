55e87451f14bbe4ee18bbe25de7b3e2622d020ed
Will Long <will@kerasai.com>
2015-08-24 07:03:15 -0500

    MIDCAMP-229: fixed selenium jar file name

--- ci/install_selenium.sh
+++ ci/install_selenium.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 wget http://selenium-release.storage.googleapis.com/2.47/selenium-server-standalone-2.47.1.jar
-java -jar selenium-server-standalone-2.31.0.jar > /dev/null &
+java -jar selenium-server-standalone-2.47.1.jar > /dev/null &
 sleep 5
