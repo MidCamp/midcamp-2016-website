c57a405eeb3927227eb0d704ef2a0d242ba272de
Will Long <will@kerasai.com>
2015-10-21 09:14:38 -0500

    MIDCAMP-239: adjusted selenium log locations

--- ci/install_selenium.sh
+++ ci/install_selenium.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 wget http://selenium-release.storage.googleapis.com/2.47/selenium-server-standalone-2.47.1.jar
-touch /var/log/selenium/output.log
-touch /var/log/selenium/error.log
-java -jar selenium-server-standalone-2.47.1.jar -port 4444 -trustAllSSLCertificates > /var/log/selenium/output.log 2> /var/log/selenium/error.log &
+touch ./selenium_output.log
+touch ./selenium_error.log
+java -jar selenium-server-standalone-2.47.1.jar -port 4444 -trustAllSSLCertificates >  ./selenium_output.log 2> ./selenium_error.log &
 
 sleep 5
