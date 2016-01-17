b87eb76f7e6ed2462c2bd73bf76fda696e87d14a
Matt Glaman <nmd.matt@gmail.com>
2016-01-17 01:11:44 -0600

    Work on Eventbrite integration

--- ci/install_drupal_mysql.sh
+++ ci/install_drupal_mysql.sh
@@ -2,4 +2,6 @@
 
 cd docroot
 chown -R www-data sites/default
+echo "\$conf['midcamp_eventbrite_oauth_token'] = getenv('TRAVIS_EVENTBRITE_OAUTH');" >> sites/default/settings.php
+echo "\$conf['midcamp_eventbrite_event_id'] = getenv('TRAVIS_EVENTBRITE_EVENT_ID');" >> sites/default/settings.php
 $BASEDIR/ci/vendor/bin/drush si -y midcamp --db-url=mysql://root@localhost/drupal
